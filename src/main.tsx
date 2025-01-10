3afc75272e944b8d6132559c70e84419fee2984d
JASH GUSANI~ <jashgusani20@gmail.com>
2025-01-11 00:20:17 +0530

    Changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './Styles/app.scss'
 import {Provider} from 'react-redux'
-import { store } from './redux/store.ts'
+import { store } from './Redux/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
